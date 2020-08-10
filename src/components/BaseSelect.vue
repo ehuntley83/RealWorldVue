d02855edda55c6ab5c2b21afe26ae5f7ffd88b9c
Ernest Huntley <ehuntley@pop-os.localdomain>
2020-08-10 15:31:08 -0400

    Adding a form field mixin for common values

--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -10,18 +10,15 @@
 </template>
 
 <script>
+import { formFieldMixin } from '../mixins/formFieldMixin';
+
 export default {
-  inheritAttrs: false,
+  mixins: [formFieldMixin],
   props: {
-    label: {
-      type: String,
-      default: ''
-    },
     options: {
       type: Array,
       required: true
-    },
-    value: [String, Number]
+    }
   },
   computed: {
     listeners() {
@@ -29,11 +26,6 @@ export default {
         ...this.$listeners,
         input: this.updateValue
       };
-    }
-  },
-  methods: {
-    updateValue(event) {
-      this.$emit('input', event.target.value);
     }
   }
 };
